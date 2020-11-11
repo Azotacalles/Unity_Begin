b50526f5eb4e9b2c46d897f3ca8eab833f40af04
Pavel <paha-r@yandex.ru>
2020-11-12 00:35:15 +0300

    Add Jump

--- Assets/Scripts/MyPlayerMovement.cs
+++ Assets/Scripts/MyPlayerMovement.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class MyPlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float jump;
     [SerializeField] private float speed;
     private Vector3 direction;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     private void Update()
     {
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector3.up * jump, ForceMode.Force);
     }
 
     private void FixedUpdate()
