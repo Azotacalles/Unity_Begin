2de2d9d08e24c9be23dc1ebc9676133b71d8c4ed
Pavel <paha-r@yandex.ru>
2020-11-11 20:12:35 +0300

    Spawn Enemy

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject pointSpawnEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] pointsSpawnEnemy = GameObject.FindGameObjectsWithTag("PointSpawnEnemy");
+        foreach (var item in pointsSpawnEnemy)
+        {
+            Instantiate(pointSpawnEnemy, item.transform.position, item.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
