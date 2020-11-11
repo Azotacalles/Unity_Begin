07da5f2d1bfe6d26f8948cc418cbe3952137645a
Pavel <paha-r@yandex.ru>
2020-11-11 19:55:50 +0300

    Player Movement

---
+++ Assets/Scripts/MyPlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 direction;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        var newSpeed = direction * speed * Time.fixedDeltaTime;
+        transform.Translate(newSpeed);
+    }
+}
