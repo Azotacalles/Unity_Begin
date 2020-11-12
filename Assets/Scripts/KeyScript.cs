6f062c1c539032a5a228baee89bd3ebd9cc899c4
Pavel <paha-r@yandex.ru>
2020-11-12 13:19:39 +0300

    Open Door, add Key and Player Inventory
    Дверь открывается, если подобрать нужный ключ.

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    [SerializeField] private int number;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerInventory>().Keys[number] = true;
+            print($"Add {number} key");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] private int amountKey;
+    private bool[] keys;
+
+    public bool[] Keys
+    {
+        get { return keys; }
+        set { keys = value; }
+    }
+
+    void Start()
+    {
+        keys = new bool[amountKey];
+    }
+
+    void Update()
+    {
+        
+    }
+}
